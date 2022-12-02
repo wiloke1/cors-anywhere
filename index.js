d581d54b1ec87baa83967f28e308a5d64b0c81c8
wiloke1 <hpvanlong@gmail.com>
2022-12-02 19:47:36 +0700

    test

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 
 createServer({
-  originWhitelist: ["*"], // Allow all origins
-  requireHeader: ["origin", "x-requested-with"],
-  removeHeaders: ["cookie", "cookie2"],
+  originWhitelist: [], // Allow all origins
+  requireHeader: [],
+  removeHeaders: [],
 }).listen(port, host, function () {
   console.log("Running CORS Anywhere on " + host + ":" + port);
 });
